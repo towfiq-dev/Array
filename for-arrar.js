c6fcac68063ebb42f926d61f927ca229042dadf8
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-30 11:08:59 +0600

    add string

---
+++ for-arrar.js
@@ -0,0 +1,15 @@
+// let myArray = ['sani', 'roni', 'moni', 'toni']
+// for(let i =0; i<=myArray.length; i+=1){
+//   console.log(myArray[i])
+// }
+// // let fd = ['sani', 'roni', 'moni', 'toni']
+// // let x =0;
+// // while(fd<=fd.length){
+// //   console.log(fd)
+// //   fd+=1
+// // }
+
+let myArray = ['sani', 'roni', 'moni', 'toni']
+for(let i =0; i<myArray.length; i+=1){
+  console.log(myArray[i])
+}
---
+++ string.js
@@ -0,0 +1,7 @@
+let str = 'Programming Hero'
+console.log(str.length)
+let str2 ='Towfiqul Islam'
+console.log(str2.length)
+console.log(str2[0])
+console.log(str[1])
+console.log('str' === 'str2')
