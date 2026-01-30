425b4f071e31c73879c464bd4a4ca3ccb652b8a9
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-30 10:15:25 +0600

    updated

---
+++ array.js
@@ -0,0 +1,11 @@
+//let myArray = [1,2,3,4,5,6,7]
+// let fd = ['sani', 'roni', 'moni', 'toni']
+// fd[3] =12
+// console.log(fd)
+//console.log(fd[0])
+//console.log(fd[3])
+
+let score = ['abc', 'xyz', 'mno', 'jkl']
+score[2]='saiful'
+console.log(score)
+//console.log(score[0])
--- push-unshift.js
+++ push-unshift.js
@@ -1,1 +1,25 @@
-let myArray = [1,2,3,4,5,6,7]
+//let myArray = [1,2,3,4,5,6,7]
+// let fd = ['sani', 'roni', 'moni', 'toni']
+// fd.push('mariya')
+// fd.unshift('saiful')
+// console.log(fd)
+
+// let friend = [10,11,12,13,14,15,16,17]
+// friend.push(18)
+// console.log(friend)
+// friend.push(19)
+// console.log(friend)
+// friend.unshift(9)
+// console.log(friend)
+// friend.unshift(8)
+// console.log(friend)
+
+let friend = [10,11,12,13,14,15,16,17]
+friend.pop()
+console.log(friend)
+friend.pop()
+console.log(friend)
+friend.shift()
+console.log(friend)
+friend.shift()
+console.log(friend)
