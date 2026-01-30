f4d9306950b23e238d667fed8f1468de6d292e4c
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-01-30 11:38:14 +0600

    string update

--- string.js
+++ string.js
@@ -1,7 +1,21 @@
-let str = 'Programming Hero'
-console.log(str.length)
-let str2 ='Towfiqul Islam'
-console.log(str2.length)
-console.log(str2[0])
-console.log(str[1])
-console.log('str' === 'str2')
+// let str = 'Programming Hero'
+// console.log(str.length)
+// let str2 ='Towfiqul Islam'
+// console.log(str2.length)
+// console.log(str2[0])
+// console.log(str[1])
+//console.log('str' === 'str2')
+
+let ph1 = 'Programming Hero'
+let ph2 = 'Programming hero'
+
+let result = ph1.toUpperCase() === ph2.toUpperCase()
+console.log(result)
+
+let ph3 = 'Programming Hero'
+let ph4 = ph3.toUpperCase()
+console.log(ph4)
+let ph5 = 'PrograMMing Hero'
+let ph6 = ph5.toUpperCase()
+console.log(ph6)
+console.log(ph4 === ph6)
